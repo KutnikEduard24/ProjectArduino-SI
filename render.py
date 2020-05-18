8b7c3f5fdaaf06b80d59d2b01c7080020b0929f5
Kutnik Eduard <eduard.kutnik@gmail.com>
2020-05-18 19:23:58 +0300

    python script

--- render.py
+++ render.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
-import logging
-import requests
 
 
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Define a route for the default URL, which loads the form
+
 @app.route("/")
 def form():
-   # f = open("date.txt","r")
-    #data = f.readline()
     with open('date.txt') as f:
         data = []
         lines = f.read()
@@ -18,7 +13,7 @@ def form():
         for i in data:
             return render_template('index.html', variable=data[-1])
 
-if __name__ == "__main2__":
-    app.run()
 
-#USER AUTH
+
+if __name__ == "__main__":
+    app.run()
